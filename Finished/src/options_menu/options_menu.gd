76246ce516cbf8c7e7260ce282a0f87a2a3d4daa
PunchablePlushie <64180414+PunchablePlushie@users.noreply.github.com>
2021-09-02 10:26:38 +0430

    Updated the project to the latest GGS version.

--- Finished/src/options_menu/options_menu.gd
+++ Finished/src/options_menu/options_menu.gd
@@ -1,10 +1,20 @@
 extends Control
 
-# You can use 'grab_focus()' to focus on the first element of the menu
+onready var FirstFocus = get_node("Mrgn/Pnl/VBox/Settings/Mrgn/VBox/Display/WindowMode/Bool")
+
+
 func _ready() -> void:
 	get_tree().paused = true
+	FirstFocus.grab_focus()
 
 
 func _on_Close_pressed() -> void:
 	get_tree().paused = false
 	queue_free()
+
+
+func _input(event: InputEvent) -> void:
+	if (event is InputEventKey) and (event.is_pressed() ):
+		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	if (event is InputEventMouseMotion):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
